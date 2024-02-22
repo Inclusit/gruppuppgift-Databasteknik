0a9bb571db3eaae4c0319e42394f3e22964b58a3
Oa Blom <oablom@gmail.com>
2024-02-22 10:25:31 +0100

    added case 2

--- main.js
+++ main.js
@@ -3,97 +3,104 @@ import prompt from "prompt-sync";
 
 try {
   const con = await connect("mongodb://127.0.0.1:27017/onlineStore");
-  
-   if (con) {
+
+  if (con) {
     console.log("Connected to MongoDB");
 
-    //Schemas 
-      const categorySchema = mongoose.Schema({
+    //Schemas
+    const categorySchema = mongoose.Schema({
+      name: { type: String },
+      description: { type: String },
+    });
+
+    const supplierSchema = mongoose.Schema({
+      name: { type: String },
+      contact: {
         name: { type: String },
-        description: { type: String },
-      });
+        email: { type: String },
+      },
+    });
 
-      const supplierSchema = mongoose.Schema({
-        name: { type: String },
-        contact: {
-          name: { type: String },
-          email: { type: String },
+    const productSchema = mongoose.Schema({
+      name: { type: String },
+      category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+      },
+      type: { type: String },
+      price: { type: Number },
+      cost: { type: Number },
+      stock: { type: Number },
+      supplier: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Supplier",
+      },
+    });
+
+    const offerSchema = mongoose.Schema({
+      products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+      price: { type: Number },
+      active: { type: Boolean },
+    });
+
+    const orderSchema = mongoose.Schema({
+      products: [
+        {
+          product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+          quantity: { type: Number, required: true },
         },
-      });
+      ],
+      offer: { type: mongoose.Schema.Types.ObjectId, ref: "Offer" },
+      quantity: { type: Number, required: true },
+      status: {
+        type: String,
+        enum: ["pending", "shipped"],
+        default: "pending",
+      },
+    });
 
-      const productSchema = mongoose.Schema({
-        name: { type: String },
-        category: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Category",
-        },
-        type: { type: String },
-        price: { type: Number },
-        cost: { type: Number },
-        stock: { type: Number },
-        supplier: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Supplier",
-        },
-      });
+    const Category = mongoose.model("Categories", categorySchema);
+    const Supplier = mongoose.model("Suppliers", supplierSchema);
+    const Products = mongoose.model("Products", productSchema);
+    const Offer = mongoose.model("Offers", offerSchema);
+    const Order = mongoose.model("Orders", orderSchema);
 
-      const offerSchema = mongoose.Schema({
-        products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
-        price: { type: Number },
-        active: { type: Boolean },
-      });
+    const { db } = mongoose.connection;
 
-      const orderSchema = mongoose.Schema({
-        products: [
-          {
-            product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
-            quantity: { type: Number, required: true },
-          },
-        ],
-        offer: { type: mongoose.Schema.Types.ObjectId, ref: "Offer" },
-        quantity: { type: Number, required: true },
-        status: {
-          type: String,
-          enum: ["pending", "shipped"],
-          default: "pending",
-        }
-      });
-
-      const Category = mongoose.model("Categories", categorySchema);
-      const Supplier = mongoose.model("Suppliers", supplierSchema);
-      const Products = mongoose.model("Products", productSchema);
-      const Offer = mongoose.model("Offers", offerSchema);
-      const Order = mongoose.model("Orders", orderSchema);      
-
-      const { db } = mongoose.connection;
-
-     async function Menu() {
-
+    async function Menu() {
       let p = prompt();
       let runApp = true;
 
       while (runApp) {
         console.log("\n ---------------------------------------------\n");
-        console.log("1. Add new category \n 2. Add new product \n 3. View products by category \n 4. View products by supplier")
-        console.log("5. View all offers within a price range \n 6. View all offers that contain a product from a specific category")
-        console.log("7. View the number of offers based ont he number of it's products in stock \n 8. Create order for products")
-        console.log("9. Create order for offers \n 10. Ship orders \n 11. Add a new supplier \n 12. View suppliers")
-        console.log("13. View all sales \n 14. View sum of all profits \n 15. Cancel")
+        console.log(
+          "1. Add new category \n 2. Add new product \n 3. View products by category \n 4. View products by supplier"
+        );
+        console.log(
+          "5. View all offers within a price range \n 6. View all offers that contain a product from a specific category"
+        );
+        console.log(
+          "7. View the number of offers based ont he number of it's products in stock \n 8. Create order for products"
+        );
+        console.log(
+          "9. Create order for offers \n 10. Ship orders \n 11. Add a new supplier \n 12. View suppliers"
+        );
+        console.log(
+          "13. View all sales \n 14. View sum of all profits \n 15. Cancel"
+        );
         console.log("\n ---------------------------------------------\n");
-        let input = p("Please make a choice by entering a number: ")
+        let input = p("Please make a choice by entering a number: ");
 
         //idé för offers:
         //"For him & her: Order one top from men's clothing and one top from women's clothing"
         //Köp tre betala för två <--- Troligtvis mycket lättare än den ovan
-        
+
         //View products  by supplier -> använd $group?
 
         //Punkt 5: Användare för välja mellan färdiginställda ranges, ex. 0-99, 100-199, 200-299. Använd sedan aggregations
         //Punkt 10: Lista orders, välj order, välj mellan om den ska vara true eller false (shipped/pending)
 
         //Create order for products -> lista alla produktnamn och pris, när någon väljer topp från män eller kvinnor så sker en offer
-
-
 
         switch (input) {
           case "1":
@@ -133,6 +140,162 @@ try {
 
           case "2":
             //massa kod
+            console.log("\n --------- Add new product --------- \n");
+            let addProduct = p("Do you wish to add a new product? y/n: ");
+
+            if (addProduct == "y") {
+              try {
+                let productName = "";
+                while (!productName) {
+                  productName = p("Input product name: ");
+                  if (!productName) {
+                    console.log(
+                      "Product name cannot be empty. Please try again."
+                    );
+                  }
+                }
+
+                let productType = "";
+                while (!productType) {
+                  productType = p("Input product type: ");
+                  if (!productType) {
+                    console.log(
+                      "Product type cannot be empty. Please try again."
+                    );
+                  }
+                }
+
+                let productPrice = NaN;
+                while (isNaN(productPrice)) {
+                  productPrice = parseFloat(p("Input product price: "));
+                  if (isNaN(productPrice)) {
+                    console.log(
+                      "Invalid input for product price. Please enter a valid number."
+                    );
+                  }
+                }
+
+                let productCost = NaN;
+                while (isNaN(productCost)) {
+                  productCost = parseFloat(p("Input product cost: "));
+                  if (isNaN(productCost)) {
+                    console.log(
+                      "Invalid input for product cost. Please enter a valid number."
+                    );
+                  }
+                }
+
+                let productStock = NaN;
+                while (isNaN(productStock)) {
+                  productStock = parseInt(p("Input product stock: "));
+                  if (isNaN(productStock)) {
+                    console.log(
+                      "Invalid input for product stock. Please enter a valid number."
+                    );
+                  }
+                }
+
+                const categoryList = await Category.find();
+                console.log("\n --------- Available Categories --------- \n");
+                categoryList.forEach((category, i) => {
+                  console.log(`${i + 1}. ${category.name}`);
+                });
+                console.log(`${categoryList.length + 1}. Add new category`);
+
+                let chosenCategory = "x";
+                while (
+                  isNaN(chosenCategory) ||
+                  chosenCategory < 1 ||
+                  chosenCategory > categoryList.length + 1
+                ) {
+                  chosenCategory = p("Choose category: ");
+                  if (isNaN(chosenCategory)) {
+                    console.log("Invalid input, please try again");
+                  }
+                }
+
+                if (
+                  chosenCategory >= 1 &&
+                  chosenCategory <= categoryList.length
+                ) {
+                  chosenCategory = categoryList[chosenCategory - 1];
+                } else if (chosenCategory == categoryList.length + 1) {
+                  let addCat = p("Do you wish to add a new category? y/n: ");
+
+                  if (addCat == "y") {
+                    try {
+                      let catName = p("Input new category name: ");
+                      let catDesc = p("Input category description: ");
+
+                      let checkExistingCat = await Category.countDocuments({
+                        name: catName,
+                      });
+
+                      if (checkExistingCat > 0) {
+                        console.log(
+                          ` Category ${catName} already exists in database, redirecting you to main menu`
+                        );
+                        break;
+                      } else {
+                        await Category.create({
+                          name: catName,
+                          description: catDesc,
+                        });
+
+                        console.log(` \n ${catName} category has been added`);
+                      }
+                    } catch (error) {
+                      console.log("Unable to add new category ", error);
+                    }
+                  } else {
+                    console.log("Redirecting you to main menu");
+                  }
+                }
+                let supplierList = await Supplier.find();
+                console.log("\n --------- Available Suppliers --------- \n");
+                supplierList.forEach((supplier, i) => {
+                  console.log(`${i + 1}. ${supplier.name}`);
+                });
+
+                let chosenSupplier = parseInt(p("Choose supplier: "));
+
+                if (
+                  chosenSupplier >= 1 &&
+                  chosenSupplier <= supplierList.length
+                ) {
+                  chosenSupplier = supplierList[chosenSupplier - 1];
+
+                  let productCreated = await Products.create({
+                    name: productName,
+                    category: chosenCategory._id,
+                    type: productType,
+                    price: productPrice,
+                    cost: productCost,
+                    stock: productStock,
+                    supplier: chosenSupplier,
+                  });
+
+                  productCreated &&
+                    console.log(
+                      `\n ${productName} has been added to ${chosenCategory.name} category`
+                    );
+                  console.log("Product created: ", productCreated);
+                } else {
+                  console.log("Invalid input, redirecting you to main menu");
+                }
+              } catch (error) {
+                console.log("Unable to add new product ", error);
+              }
+            }
+            //massa kod
+
+            //struktur för om någon vill lägga till ny produkt:
+            //"choose category"-> lista med kategorier
+            //"category not available, do you want to create a new one?" -> "redirecting you to category creation"
+
+            //"choose category" -> "insert x" -> "you have added x to y category"
+
+            break;
 
             //struktur för om någon vill lägga till ny produkt:
             //"choose category"-> lista med kategorier
@@ -207,12 +370,12 @@ try {
                 console.log(
                   `\nThere are currently no products in ${chosenSupplier.name}, redirecting you to main menu`
                 );
-              } 
+              }
 
               break;
             } else {
               console.log("\nInvalid input, redirecting you to main menu");
-            } 
+            }
             break;
 
           case "5":
@@ -310,19 +473,11 @@ try {
             );
             break;
         } //End of switch/case loop
-      
-    }; //End of runApp loop
-      
-     }//End of async menu function
+      } //End of runApp loop
+    } //End of async menu function
 
-     Menu();
-
-    }; //End of if-function
-
-
+    Menu();
+  } //End of if-function
 } catch (error) {
   console.log("Error connecting to MongoDB:", error);
-  
 }
-
-
